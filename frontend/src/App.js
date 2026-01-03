3f1ece344e97da485ac4973e434202f4c26c62f5
Aishwarya <aishwaryagkamkar0919@gmail.com>
2026-01-03 17:59:17 +0530

    Fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [editingId, setEditingId] = useState(null);
   const [showAll, setShowAll] = useState(false);
 
-  const API_URL = "https://contactmanager-backend-m5nb.onrender.com";
+  const API_URL = "https://contactmanager-backend-m5nb.onrender.com/api/contacts";
 
   // LOAD CONTACTS
   const loadContacts = async () => {
